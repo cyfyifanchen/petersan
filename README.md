adbf563c50b927753cde88ab334d666034f654bc
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-12-08 16:31:58 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Introduction
+
+Petersan(named after one of my friends), is a starter template with NextJS 14 App router and StyleX integrated. I would normally use Tailwind for styling, just for the sake of easiness. But I do admit writing CSS is more creative than using class based Tailwind.
+
+### Run local
+
+```
+npm install
+npm run dev
+```
---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const stylexPlugin = require("@stylexjs/nextjs-plugin");
+const withMDX = require("@next/mdx")();
+
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
+};
+
+module.exports = stylexPlugin({
+  rootDir: __dirname,
+})(withMDX(nextConfig));
